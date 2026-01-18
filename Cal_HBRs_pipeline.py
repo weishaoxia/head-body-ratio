f598c4228223ac150a4712c55be8b946b707f954
weishaoxia <646707254@qq.com>
2026-01-19 00:13:08 +0800

    Update Cal_HBRs_pipeline.py

--- Cal_HBRs_pipeline.py
+++ Cal_HBRs_pipeline.py
@@ -1,1 +1,23 @@
-
+from tqdm import tqdm
+import random
+import os
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+import sys
+import shutil
+import json
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, Subset, DataLoader, random_split
+import torchvision
+import torchvision.models as models
+from torchvision import transforms
+from torchvision.utils import save_image
+from sklearn.metrics import roc_auc_score
+import segmentation_models_pytorch as smp
+from PIL import Image, ImageDraw 
+import cv2
